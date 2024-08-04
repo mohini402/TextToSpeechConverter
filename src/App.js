3ae0a16887f6793bf0fcb14fe3f6bd70f7f212d6
mohini agarwal <mohiniagarwal1408@gmail.com>
2024-08-05 00:40:37 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { useSpeechSynthesis } from 'react-speech-kit'; //hook for speech
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [voiceIndex, setVoiceIndex] = useState(null);
+  const [rate, setRate]=useState(0.1);
+  const { speak, voices } = useSpeechSynthesis();
+
+  // Function to handle the speak action
+  function handleSpeak() {
+    const voice = voices[voiceIndex] || null;
+    speak({ text: inputText, rate: rate, voice });
+  }
+
+  // Return JSX
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Text To Speech Converter</h1>
+      <textarea
+        cols="80"
+        rows="20"
+        placeholder="Enter The Text"
+        onChange={(e) => setInputText(e.target.value)}
+      ></textarea><br />
+
+      <select onChange={(e) => setVoiceIndex(e.target.value)} value={voiceIndex || ''}>
+        <option value="">default</option>
+        {voices.map((item, index) => (
+          <option key={item.name} value={index}>
+            {item.name}
+          </option>
+        ))}
+      </select>
+
+      <div id='slider'>
+        <input type='range' min='0.1' max='1' step='0.01' value={rate} onChange={(e)=>setRate(e.target.value)}/>
+      </div>
+      <h3>Current Speech Rate is {rate}</h3>
+      <button id='btn' onClick={handleSpeak}>Speech</button>
     </div>
   );
 }
